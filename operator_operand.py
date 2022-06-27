7091b4436db9bdc17cd92e7eb9232e603de50265
winnerkypt <kanyapat.winnerkypt@mail.kmutt.ac.th>
2022-06-27 15:48:18 +0700

    operator and operand

---
+++ operator_operand.py
@@ -0,0 +1,19 @@
+"""
+operator ตัวดำเนินการ
+operand ตัวถูกดำเนินการ
+
+A + B
++ คือ operator
+A, B คือ operand
+"""
+
+x = 5
+y = 10
+
+print("ผลบวก", x+y)
+print("ผลลบ", x-y)
+print("ผลคูณ", x*y)
+print("ผลหาร" , x/y)
+print("หารปัดเศษ", x//y)
+print("ยกกำลัง" , 5**2)
+print("หารเอาเศษ", 10%4)
