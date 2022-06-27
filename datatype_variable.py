236cf25d1741a6f0c6b7b6d9c77ec046fce10f74
winnerkypt <kanyapat.winnerkypt@mail.kmutt.ac.th>
2022-06-27 11:49:50 +0700

    datatype and variable

---
+++ datatype_variable.py
@@ -0,0 +1,12 @@
+# ชื่อตัวแปร = ค่าที่เก็บในตัวแปร
+age = 19
+name = "Kanyapat"
+isCorrect = True
+print("result : "+ str(age)) # แปลงเป็น String โดยใช้ str()
+print("name : " + name)
+print(isCorrect)
+
+# ตรวจสอบว่าเป็นชนิดข้อมูลอะไร
+print(type(age)) 
+print(type(name))
+print(type(isCorrect))
